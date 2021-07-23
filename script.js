9ae54ef514b4eebbc626277b40979df840381fc5
BeardedBird <yed_nc@outlook.es>
2021-07-23 14:42:40 -0500

    js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function compute()
 {
     var amount = document.getElementById("principal").value || 0
-    var rate = document.getElementById("rate").value 
-    var years = document.getElementById('years').value
-    
-    document.getElementById('result-content').style.display ="inline" 
-    
-    document.getElementById('r_amount').innerHTML = amount
-    document.getElementById('r_interest').innerHTML = rate
-    document.getElementById('result').innerHTML = amount*rate/100*years
-    document.getElementById('r_date').innerHTML = new Date().getFullYear() + parseInt(years)
+    if (amount<=0){
+        alert("Please insert a positive number in the amount field");
+        document.getElementById("principal").focus();
+    }
+    else{
+        var rate = document.getElementById("rate").value 
+        var years = document.getElementById('years').value
+        
+        document.getElementById('result-content').style.display ="inline" 
+        
+        document.getElementById('r_amount').innerHTML = amount
+        document.getElementById('r_interest').innerHTML = rate
+        document.getElementById('result').innerHTML = amount*rate/100*years
+        document.getElementById('r_date').innerHTML = new Date().getFullYear() + parseInt(years)
+    }
 }
         
 function showValue(val) {
