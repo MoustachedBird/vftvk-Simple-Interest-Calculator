fd49eb799be80620a947c89ff243d99f51efc852
BeardedBird <yed_nc@outlook.es>
2021-07-23 14:16:14 -0500

    project completed

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var amount = document.getElementById("principal").value || 0
+    var rate = document.getElementById("rate").value 
+    var years = document.getElementById('years').value
     
+    document.getElementById('result-content').style.display ="inline" 
+    
+    document.getElementById('r_amount').innerHTML = amount
+    document.getElementById('r_interest').innerHTML = rate
+    document.getElementById('result').innerHTML = amount*rate/100*years
+    document.getElementById('r_date').innerHTML = new Date().getFullYear() + parseInt(years)
 }
         
+function showValue(val) {
+    document.getElementById('percentage').innerHTML = val
+}
